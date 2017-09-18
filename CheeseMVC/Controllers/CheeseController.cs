d5405a936d2f8e4fed0493dee1599511f3ea562c
Nicholas Montanino <montanino@utexas.edu>
2017-09-18 10:42:21 -0400

    Add description form and modify layout.

--- CheeseMVC/Controllers/CheeseController.cs
+++ CheeseMVC/Controllers/CheeseController.cs
@@ -4,13 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace CheeseMVC.Controllers
 {
     public class CheeseController : Controller
     {
-        static private List<string> Cheeses = new List<string>();
+        static private Dictionary<string, string> Cheeses = new Dictionary<string, string>();
 
         // GET: /<controller>/
         public IActionResult Index()
@@ -27,10 +25,10 @@ namespace CheeseMVC.Controllers
 
         [HttpPost]
         [Route("Cheese/Add")]
-        public IActionResult NewCheese(string name)
+        public IActionResult NewCheese(string name, string description)
         {
             // Add new cheese to existing cheeses
-            Cheeses.Add(name);
+            Cheeses.Add(name, description);
 
             return Redirect("/Cheese");
         }

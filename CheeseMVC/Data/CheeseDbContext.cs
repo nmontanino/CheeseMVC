ee702a17f640b3a31ca24ee30f91237e829e3360
Nicholas Montanino <montanino@utexas.edu>
2017-10-04 22:42:21 -0400

    Configure project to connect to SQL database.

---
+++ CheeseMVC/Data/CheeseDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CheeseMVC.Models;
+
+namespace CheeseMVC.Data
+{
+    public class CheeseDbContext : DbContext
+    {
+
+        public DbSet<Cheese> Cheeses { get; set; }
+
+        public CheeseDbContext(DbContextOptions<CheeseDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
